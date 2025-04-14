ac772c76174249059f586e666a47a5732a04a892
ClaudioFMattos <claudiofmattosgpe@gmail.com>
2025-04-13 21:47:34 -0300

    feat: pages and components add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+import Notification from "@/components/Notification";
+
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +32,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Notification />
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-red-500 text-white px-4 py-3 flex items-center justify-center text-center text-sm md:txt-base cursor-pointer'>Taxa de entrega gratuita nos pedidos acima de R$ 50,00</div>
+  )
+}
+
+export default Notification
